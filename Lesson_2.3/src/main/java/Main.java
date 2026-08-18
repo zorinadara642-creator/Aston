4c7bdec8ca6237673eb37f893590980868473910
Darya <zorinadara642@gmail.com>
2026-08-18 11:45:52 +0400

    Reformat code

--- Lesson_2.3/src/main/java/Main.java
+++ Lesson_2.3/src/main/java/Main.java
@@ -3,35 +3,35 @@ public class Main {
         Product[] productsArray = new Product[5];
         System.out.println("Задание 1-2");
 
-        productsArray [0] = new Product("Samsun s25",
+        productsArray[0] = new Product("Samsun s25",
                 "01.02.2025",
                 "Samsung Corp",
                 "Korea",
                 5599,
                 true);
 
-        productsArray [1] = new Product("iphone 16 Pro",
+        productsArray[1] = new Product("iphone 16 Pro",
                 "15.09.2024",
                 "Apple Inc.",
                 "USA",
                 88500,
                 false);
 
-        productsArray [2] = new Product("Xiaomi 14",
+        productsArray[2] = new Product("Xiaomi 14",
                 "10.11.2024",
                 "Xiaomi",
                 "China",
                 3499,
                 true);
 
-        productsArray [3] = new Product("Sony 3000",
+        productsArray[3] = new Product("Sony 3000",
                 "24.05.2023",
                 "Sony",
                 "Japan",
                 12999,
                 false);
 
-        productsArray [4] = new Product("Lenovo 2500",
+        productsArray[4] = new Product("Lenovo 2500",
                 "03.03.2024",
                 "Lenovo",
                 "China",
