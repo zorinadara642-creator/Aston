1c059079f460f76baebeb5c4408c5e791d382c8a
Darya <zorinadara642@gmail.com>
2026-09-07 15:11:02 +0400

    JUnit 5

---
+++ Lesson_7/src/main/java/NumberComparator.java
@@ -0,0 +1,12 @@
+public class NumberComparator {
+
+    public int compare(int a, int b) {
+        if (a > b) {
+            return 1;
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
